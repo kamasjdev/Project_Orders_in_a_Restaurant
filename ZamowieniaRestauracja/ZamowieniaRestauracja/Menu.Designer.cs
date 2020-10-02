51a5c19c57c9713aa6aa295d824ae1decd4973c9
KW <KW*KW>
2020-10-02 21:41:43 +0200

    małe poprawki

--- ZamowieniaRestauracja/ZamowieniaRestauracja/Menu.Designer.cs
+++ ZamowieniaRestauracja/ZamowieniaRestauracja/Menu.Designer.cs
@@ -72,11 +72,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(139, 57);
+            this.label2.Location = new System.Drawing.Point(139, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.Size = new System.Drawing.Size(255, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Menu:";
+            this.label2.Text = "Wybierz produkt, który chcesz zamówić";
             // 
             // comboBoxAdditions
             // 
